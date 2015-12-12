2f4b63e079231293a1f4240185d539463512724b
bradhouser <forgivingcomputers@gmail.com>
2015-12-12 08:42:23 -0600

    new comments for github

--- app/src/androidTest/java/com/forgivingcomputers/calctest2/ApplicationTest.java
+++ app/src/androidTest/java/com/forgivingcomputers/calctest2/ApplicationTest.java
@@ -4,6 +4,8 @@ import android.app.Application;
 import android.test.ApplicationTestCase;
 
 // java comment
+//first branch comment added here
+
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
